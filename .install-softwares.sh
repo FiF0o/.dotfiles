52c3580c5f447dfc793ddf426377c9f6cdced35d
Jon <lazarini.jonathan@gmail.com>
2019-06-10 23:14:56 +0100

    add more apps to be installesd

--- .install-softwares.sh
+++ .install-softwares.sh
@@ -5,5 +5,27 @@ echo 'Installing apps ...'
 
 brew cask install whatsapp
 brew cask install google-chrome
+brew cask install charles
+brew cask install arduino
+
+### TODO softwares without brew formulae :(
+# Adobe
+# Affinity Designer.app
+# Affinity Photo.app
+# AirParrot 2.app
+# Android File Transfer.app
+# Canon Utilities
+# Cisco Jabber.app
+# Framer Studio.app
+# GraphQL
+# Hype2.app ?
+# Kaleidoscope.app ?
+# OmniGraffle.app ?
+# ScreenFlow.app
+# Silverback.app
+# Skala Preview.app ?
+# TheBrain.app
+# xSort.app
+# zoom.us.app
 
 echo '... Apps installation finished'
