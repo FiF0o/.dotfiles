916c408d5115e2467e572a862aaeac57bd7b1b7a
FiF0o <jonathan.lazarini@wipro.com>
2019-12-03 16:15:22 +0000

    add vue vscode extensions

--- .install-vscode-extensions.sh
+++ .install-vscode-extensions.sh
@@ -14,3 +14,4 @@ code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension VisualStudioExptTeam.vscodeintellicod
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension octref.vetur
